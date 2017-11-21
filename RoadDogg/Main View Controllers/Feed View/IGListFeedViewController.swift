2ba655e092de4621f32e6902064c800dca721df6
Adrian Humphrey <aahumphrey@cslab-mac30.sbcc.local>
2017-11-20 20:31:51 -0800

    Implented IgListkit.

--- RoadDogg/Main View Controllers/Feed View/IGListFeedViewController.swift
+++ RoadDogg/Main View Controllers/Feed View/IGListFeedViewController.swift
@@ -8,12 +8,61 @@
 
 import Foundation
 import UIKit
+import IGListKit
 
 class IGListFeedViewController : UIViewController{
     
+    //Collection View
+    let collectionView: UICollectionView = {
+        let view = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.backgroundColor = UIColor.gray
+        return view
+    }()
+    
+    //IGList Adapter
+    lazy var adapter: ListAdapter = {
+        return ListAdapter(updater: ListAdapterUpdater(), viewController: self, workingRangeSize: 0)
+    }()
+    
+    //Posts that will populate CollectionView
+    var posts : [Post]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(collectionView)
+        
+        //Set up collection Adapter
+        adapter.collectionView = collectionView
+        adapter.dataSource = self
+        
+        //Grab All posts
+        fetchFeed()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
     
+    func fetchFeed(){
+        Network.shared.getFeed().then { feed -> Void in
+            self.posts = feed.posts
+        }
+    }
+}
+
+extension IGListFeedViewController: ListAdapterDataSource {
+    // 1
+    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+        return self.posts as! [ListDiffable]
+    }
+    
+    // 2
+    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        //Check if this post was posted by a rider or a driver
+        return ListSectionController()
+    }
+    
+    // 3
+    func emptyView(for listAdapter: ListAdapter) -> UIView? { return nil  } //Return a certatin view later
 }
